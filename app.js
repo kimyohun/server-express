96dafd8e9af02b473232ba0e9fab00d78ed36c89
KimYoHun <yohunkim0@gmail.com>
2025-03-12 21:51:32 +0900

    서버 오픈

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("안녕하세요");
+import http from 'node:http';
+import fs from 'node:fs';
+import qs from 'node:querystring';
+
+const server = http.createServer(function(request, response){
+
+    // GET 방식 
+    if(request.method === 'GET'){
+        if(request.url === '/' || request.url === '/index.html'){
+            const filePath = fs.readFileSync('./index.html')
+            response.writeHead(200, {'Content-Type' : 'text/html'}).end(filePath);
+        }
+    }
+
+    // POST 방식
+    if(request.method === 'POST'){
+        
+    }
+
+})
+
+server.listen(3000, function(){
+    console.log("http://localhost:3000 실행중");
+})
